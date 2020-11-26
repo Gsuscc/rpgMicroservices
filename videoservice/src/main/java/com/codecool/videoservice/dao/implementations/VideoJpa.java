1973be9b5aa49cf2579215563650cdbdac0be18b
Gergely Molnar <molnar.gergo18@gmail.com>
2020-11-26 18:33:52 +0100

    add VideoRepository

--- videoservice/src/main/java/com/codecool/videoservice/dao/implementations/VideoJpa.java
+++ videoservice/src/main/java/com/codecool/videoservice/dao/implementations/VideoJpa.java
@@ -1,4 +1,31 @@
 package com.codecool.videoservice.dao.implementations;
 
-public class VideoJpa {
+import com.codecool.videoservice.dao.interfaces.VideoDao;
+import com.codecool.videoservice.entity.Video;
+import com.codecool.videoservice.repository.VideoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class VideoJpa implements VideoDao {
+
+
+    private VideoRepository videoRepository;
+
+    @Autowired
+    public VideoJpa(VideoRepository videoRepository) {
+        this.videoRepository = videoRepository;
+    }
+
+    @Override
+    public void addVideo(Video video) {
+        videoRepository.save(video);
+    }
+
+    @Override
+    public List<Video> getAll() {
+        return videoRepository.findAll();
+    }
 }
--- videoservice/src/main/java/com/codecool/videoservice/dao/interfaces/VideoDao.java
+++ videoservice/src/main/java/com/codecool/videoservice/dao/interfaces/VideoDao.java
@@ -1,4 +1,12 @@
 package com.codecool.videoservice.dao.interfaces;
 
+import com.codecool.videoservice.entity.Video;
+
+import java.util.List;
+
 public interface VideoDao {
+
+    void addVideo(Video video);
+
+    List<Video> getAll();
 }
