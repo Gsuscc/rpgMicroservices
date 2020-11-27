40c97171abbe0f0c1046e52c14bb9baccacc0901
Gergely Molnar <molnar.gergo18@gmail.com>
2020-11-27 16:18:13 +0100

    enable eurake on VideoServiceApplication

--- videoservice/src/main/java/com/codecool/videoservice/VideoServiceApplication.java
+++ videoservice/src/main/java/com/codecool/videoservice/VideoServiceApplication.java
@@ -2,12 +2,14 @@ package com.codecool.videoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class VideoserviceApplication {
+@EnableEurekaClient
+public class VideoServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VideoserviceApplication.class, args);
+		SpringApplication.run(VideoServiceApplication.class, args);
 	}
 
 }
--- videoservice/src/test/java/com/codecool/videoservice/VideoServiceApplicationTests.java
+++ videoservice/src/test/java/com/codecool/videoservice/VideoServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class VideoserviceApplicationTests {
+class VideoServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
